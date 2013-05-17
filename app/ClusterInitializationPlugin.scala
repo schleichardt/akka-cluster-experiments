fe3fc28275b24938141af56cc7093ede63873fcd
Michael Schleichardt <michael@schleichardt.net>
2013-05-17 08:57:00 +0200

    index page shows cluster members

--- app/ClusterInitializationPlugin.scala
+++ app/ClusterInitializationPlugin.scala
@@ -1,13 +1,22 @@
 package info.schleichardt.experiments.akka.cluster
 
 import play.api.{Logger, Plugin, Application}
-import akka.actor.{Props, ActorLogging, Actor, ActorSystem}
+import akka.actor._
 import akka.cluster.ClusterEvent._
 import akka.cluster.ClusterEvent.MemberUp
 import akka.cluster.ClusterEvent.UnreachableMember
 import akka.cluster.ClusterEvent.MemberJoined
 import akka.cluster.ClusterEvent.CurrentClusterState
-import akka.cluster.Cluster
+import akka.cluster.{Member, Cluster}
+import scala.collection.immutable.SortedSet
+import akka.cluster.ClusterEvent.MemberJoined
+import akka.cluster.ClusterEvent.CurrentClusterState
+import akka.cluster.ClusterEvent.MemberLeft
+import akka.cluster.ClusterEvent.MemberRemoved
+import akka.cluster.ClusterEvent.MemberUp
+import akka.cluster.ClusterEvent.MemberExited
+import akka.cluster.ClusterEvent.MemberDowned
+import akka.cluster.ClusterEvent.UnreachableMember
 
 class ClusterInitializationPlugin(app: Application) extends Plugin {
   lazy val system = ActorSystem("ClusterSystem")
@@ -16,7 +25,18 @@ class ClusterInitializationPlugin(app: Application) extends Plugin {
     Logger.info(s"starting ${this.getClass.getName}")
     val clusterListener = system.actorOf(Props[ClusterLoggingActor], "clusterLogger")
     Cluster(system).subscribe(clusterListener, classOf[ClusterDomainEvent])
+    val clusterStateActor = system.actorOf(Props[ClusterStateActor], "clusterState")
+    Cluster(system).subscribe(clusterStateActor, classOf[ClusterDomainEvent])
   }
+
+
+  override def onStop() {
+    super.onStop()
+    Logger.info(s"stopping ${this.getClass.getName}")
+    system.shutdown()
+  }
+
+  def stateActor = system.actorFor("user/clusterState")
 }
 
 //inspired from http://doc.akka.io/docs/akka/2.1.4/cluster/cluster-usage-scala.html
@@ -30,7 +50,27 @@ class ClusterLoggingActor extends Actor {
       Logger.info("Member is Up: " + member)
     case UnreachableMember(member) ⇒
       Logger.info("Member detected as unreachable: " + member)
+    case MemberDowned(member) =>
+      Logger.info("Member is down: " + member)
+    case MemberExited(member) =>
+      Logger.info("Member exited: " + member)
+    case MemberLeft(member) =>
+      Logger.info("Member left: " + member)
     case _: ClusterDomainEvent ⇒ // ignore
 
+  }
+}
+
+case object MemberInformationQuestion
+case class MemberInformation(members: SortedSet[Member])
+
+class ClusterStateActor extends Actor {
+  var members: SortedSet[Member] = SortedSet()
+
+  def receive = {
+    case state: CurrentClusterState => members = state.members
+    case MemberUp(member) => members += member
+    case MemberRemoved(member) => members -= member
+    case MemberInformationQuestion => sender tell (MemberInformation(members), self)
   }
 }
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,24 @@
 package controllers
 
+import info.schleichardt.experiments.akka.cluster.{MemberInformation, MemberInformationQuestion, ClusterInitializationPlugin}
 import play.api._
 import play.api.mvc._
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent._
+import ExecutionContext.Implicits.global
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    implicit val timeout = Timeout(500)
+    val stateActor = Play.current.plugin(classOf[ClusterInitializationPlugin]).get.stateActor
+    val clusterInformation = (stateActor ask MemberInformationQuestion).mapTo[MemberInformation]
+    AsyncResult {
+      clusterInformation map { c =>
+        Ok(views.html.index(c))
+      }
+    }
   }
-  
+
 }
