6beae11e872fe57082db0f12770f7f5b3277beeb
Michael Schleichardt <michael@schleichardt.net>
2014-01-05 12:12:28 +0100

    Add descriptions for test cases.

--- src/test/scala/ClusterShardingSpec.scala
+++ src/test/scala/ClusterShardingSpec.scala
@@ -1,8 +1,12 @@
 import org.scalatest.{Matchers, FunSuite}
 
+/**
+ * See http://doc.akka.io/docs/akka/2.3-M2/contrib/cluster-sharding.html
+ */
 class ClusterShardingSpec extends FunSuite with Matchers {
 
-  test("1 == 1") {
-    1 should be(1)
-  }
+  test("distribute Actors to different cluster nodes and don't have to worry about physical location")(pending)
+  test("change location of an Actor over time")(pending)//stateful actors!
+  test("Proxy Only Mode")(pending)
+  test("Passivation")(pending)
 }
