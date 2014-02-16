f869efe64e2c18456803fcaa66c5a17e29de1b2d
Michael Schleichardt <michael@schleichardt.net>
2014-02-16 13:39:46 +0100

    setup cluster test

--- src/multi-jvm/scala/ClusterShardingSpec.scala
+++ src/multi-jvm/scala/ClusterShardingSpec.scala
@@ -1,119 +1,128 @@
-//from https://raw2.github.com/akka/akka/master/akka-contrib/src/multi-jvm/scala/akka/contrib/pattern/ClusterShardingSpec.scala
-
-/**
- *  Copyright (C) 2009-2013 Typesafe Inc. <http://www.typesafe.com>
- */
-package sharding
-
-import language.postfixOps
-import scala.concurrent.duration._
-import com.typesafe.config.ConfigFactory
-import akka.actor._
-import akka.cluster.Cluster
-import akka.cluster.ClusterEvent._
-import akka.persistence.EventsourcedProcessor
-import akka.persistence.Persistence
-import akka.persistence.journal.leveldb.SharedLeveldbJournal
-import akka.persistence.journal.leveldb.SharedLeveldbStore
-import akka.remote.testconductor.RoleName
-import akka.remote.testkit.MultiNodeConfig
-import akka.remote.testkit.MultiNodeSpec
-import akka.testkit._
-import akka.testkit.TestEvent.Mute
-import java.io.File
-import org.scalatest.{Matchers, FunSuiteLike}
-import akka.contrib.pattern._
-import akka.remote.testconductor.RoleName
-import scala.Some
-import akka.cluster.ClusterEvent.MemberUp
-import akka.cluster.ClusterEvent.CurrentClusterState
-import sample.cluster.counter.ShardingApp
-
-object ClusterShardingSpec extends MultiNodeConfig {
-  val controller = role("controller")
-  val first = role("first")
-  val second = role("second")
-  val third = role("third")
-
-  commonConfig(ConfigFactory.parseString(
-    """
-      |akka {
-      |   actor {
-      |     provider = "akka.cluster.ClusterActorRefProvider"
-      |   }
-      |  cluster {
-      |    seed-nodes = [
-      |      "akka.tcp://ClusterShardingSpec@localhost:2551",
-      |      "akka.tcp://ClusterShardingSpec@localhost:2552"]
-      |
-      |    auto-down-unreachable-after = 10s
-      |  }
-      |  persistence {
-      |    journal.plugin = "akka.persistence.journal.leveldb-shared"
-      |    journal.leveldb-shared.store {
-      |      native = off
-      |      dir = "target/shared-journal"
-      |    }
-      |    snapshot-store.local.dir = "target/snapshots"
-      |  }
-      |}
-      |
-    """.stripMargin))
-
-  def nodeConf(port: String) = ConfigFactory.parseString( s"akka.remote.netty.tcp.port=$port")
-
-  nodeConfig(first)(nodeConf("2551"))
-  nodeConfig(second)(nodeConf("2552"))
-  nodeConfig(third)(nodeConf("2553"))
-}
-
-class ClusterShardingMultiJvmNode1 extends ClusterShardingSpec
-class ClusterShardingMultiJvmNode2 extends ClusterShardingSpec
-class ClusterShardingMultiJvmNode3 extends ClusterShardingSpec
-class ClusterShardingMultiJvmNode4 extends ClusterShardingSpec
-
-class ClusterShardingSpec extends MultiNodeSpec(ClusterShardingSpec) with FunSuiteLike with ImplicitSender with Matchers {
-  import ClusterShardingSpec._
-
-
-  override def initialParticipants = roles.size
-
-  override protected def atStartup() {
-  }
-
-  override protected def afterTermination() {
-  }
-
-  class Stats extends Actor {
-    def receive: Actor.Receive = {
-      case x => println("received" + x)
-    }
-  }
-
-  test("startup cluster") {
-    runOn(controller){
-      enterBarrier("store started")
-      enterBarrier("started")
-    }
-    runOn(first){
-      ShardingApp.startup("2551", system)
-      enterBarrier("store started")
-      enterBarrier("started")
-    }
-    runOn(second){
-      enterBarrier("store started")
-      ShardingApp.startup("2552", system)
-      enterBarrier("started")
-      println("members " + Cluster(system).state.members)
-    }
-    runOn(third){
-      enterBarrier("store started")
-      ShardingApp.startup("2553", system)
-      enterBarrier("started")
-      println("members " + Cluster(system).state.members)
-    }
-  }
-
+//package sharding
+//
+//import language.postfixOps
+//import scala.concurrent.duration._
+//import com.typesafe.config.ConfigFactory
+//import akka.actor._
+//import akka.cluster.Cluster
+//import akka.cluster.ClusterEvent._
+//import akka.persistence.EventsourcedProcessor
+//import akka.persistence.Persistence
+//import akka.persistence.journal.leveldb.SharedLeveldbJournal
+//import akka.persistence.journal.leveldb.SharedLeveldbStore
+//import akka.remote.testconductor.RoleName
+//import akka.remote.testkit.MultiNodeConfig
+//import akka.remote.testkit.MultiNodeSpec
+//import akka.testkit._
+//import akka.testkit.TestEvent.Mute
+//import java.io.File
+//import org.scalatest.{Matchers, FunSuiteLike}
+//import akka.contrib.pattern._
+//import akka.remote.testconductor.RoleName
+//import scala.Some
+//import akka.cluster.ClusterEvent.MemberUp
+//import akka.cluster.ClusterEvent.CurrentClusterState
+//import sample.cluster.counter.ShardingApp
+//
+//object ClusterShardingSpec extends MultiNodeConfig {
+//  val controller = role("controller")
+//  val first = role("first")
+//  val second = role("second")
+//  val third = role("third")
+//
+//  def nodeConf(port: String) = ConfigFactory.parseString(
+//    s"""
+//      |akka {
+//      |  actor {
+//      |    provider = "akka.cluster.ClusterActorRefProvider"
+//      |  }
+//      |  remote {
+//      |    enabled-transports = ["akka.remote.netty.tcp"]
+//      |    log-remote-lifecycle-events = off
+//      |    netty.tcp {
+//      |      hostname = "localhost"
+//      |      port = $port
+//      |    }
+//      |  }
+//      |
+//      |  cluster {
+//      |    seed-nodes = [
+//      |      "akka.tcp://ClusterShardingSpec@localhost:2551",
+//      |      "akka.tcp://ClusterShardingSpec@localhost:2552",
+//      |      "akka.tcp://ClusterShardingSpec@localhost:2553"]
+//      |
+//      |    auto-down-unreachable-after = 10s
+//      |  }
+//      |}
+//    """.stripMargin)
+//
+//  nodeConfig(first)(nodeConf("2551"))
+//  nodeConfig(second)(nodeConf("2552"))
+//  nodeConfig(third)(nodeConf("2553"))
+//}
+//
+//class ClusterShardingMultiJvmNode1 extends ClusterShardingSpec
+//class ClusterShardingMultiJvmNode2 extends ClusterShardingSpec
+//class ClusterShardingMultiJvmNode3 extends ClusterShardingSpec
+//class ClusterShardingMultiJvmNode4 extends ClusterShardingSpec
+//
+//class ClusterShardingSpec extends MultiNodeSpec(ClusterShardingSpec) with FunSuiteLike with ImplicitSender with Matchers {
+//  import ClusterShardingSpec._
+//
+//
+//  override def initialParticipants = roles.size
+//
+//  override protected def atStartup() {
+//  }
+//
+//  override protected def afterTermination() {
+//  }
+//
+//  class Stats extends Actor {
+//    def receive: Actor.Receive = {
+//      case x => println("received" + x)
+//    }
+//  }
+//
+//  muteDeadLetters(classOf[Any])(system)
+//
+//  test("startup cluster") {
+////    println(system.settings)
+//    runOn(controller){
+//      enterBarrier("store started")
+//      enterBarrier("started")
+//    }
+//    runOn(first){
+////      ShardingApp.startup("2551", system)
+//      enterBarrier("store started")
+//      Cluster(system).join(node(first).address)
+//      Cluster(system).join(node(second).address)
+//      Cluster(system).join(node(third).address)
+//      enterBarrier("started")
+//    }
+//    runOn(second){
+//      enterBarrier("store started")
+////      ShardingApp.startup("2552", system)
+//      Cluster(system).join(node(first).address)
+//      Cluster(system).join(node(second).address)
+//      Cluster(system).join(node(third).address)
+//      enterBarrier("started")
+//      Thread.sleep(2000)
+//      println("members " + Cluster(system).state.members)
+//    }
+//    runOn(third){
+//      enterBarrier("store started")
+////      ShardingApp.startup("2553", system)
+//      Cluster(system).join(node(first).address)
+//      Cluster(system).join(node(second).address)
+//      Cluster(system).join(node(third).address)
+//      enterBarrier("started")
+//      Thread.sleep(2000)
+//      println("members " + Cluster(system).state.members)
+//    }
+//    //man muss länger warten, bis sich cluster gebildet hat!
+//  }
+//
 //  test("illustrate how to startup cluster") {
 //    runOn(controller){
 //      enterBarrier("cluster")
@@ -141,4 +150,4 @@ class ClusterShardingSpec extends MultiNodeSpec(ClusterShardingSpec) with FunSui
 //    }
 //    testConductor.enter("all-up")
 //  }
-}
+//}
---
+++ src/multi-jvm/scala/ClusterSpec.scala
@@ -0,0 +1,61 @@
+import akka.actor.{Actor, Props}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent.{MemberUp, CurrentClusterState}
+import akka.remote.testkit.{MultiNodeSpec, MultiNodeConfig}
+import akka.testkit.ImplicitSender
+import com.typesafe.config.ConfigFactory
+import multinodetest.MultiSpec
+import org.scalatest._
+import language.postfixOps
+import scala.concurrent.duration._
+
+object ClusterSpecConfig extends MultiNodeConfig {
+  val seed = role("controller")
+  val first = role("first")
+  val second = role("second")
+  val third = role("third")
+
+  commonConfig(ConfigFactory.parseString(""" akka.actor.provider="akka.cluster.ClusterActorRefProvider" """))
+}
+
+class WordsClusterSpecMultiJvmNode1 extends WordsClusterSpec
+class WordsClusterSpecMultiJvmNode2 extends WordsClusterSpec
+class WordsClusterSpecMultiJvmNode3 extends WordsClusterSpec
+class WordsClusterSpecMultiJvmNode4 extends WordsClusterSpec
+
+class WordsClusterSpec extends MultiNodeSpec(ClusterSpecConfig) with WordSpecLike with MustMatchers with MultiSpec with ImplicitSender {
+
+  import ClusterSpecConfig._
+
+  def initialParticipants = roles.size
+
+  muteDeadLetters(classOf[Any])(system)
+
+  "A cluster" must {
+    "contain of all nodes" in within(10 seconds) {
+      Cluster(system).subscribe(testActor, classOf[MemberUp])
+      expectMsgClass(classOf[CurrentClusterState])
+      Cluster(system).join(node(seed).address)
+      receiveN(4).map {
+        case MemberUp(m) => m.address
+      }.toSet must be(
+        Set(node(seed).address, node(first).address, node(second).address, node(third).address))
+      Cluster(system).unsubscribe(testActor)
+    }
+    enterBarrier("cluster-up")
+    "execute a job" in within(10 seconds) {
+      runOn(first) {
+        val actorRef = system.actorOf(Props[DemoActor1], "demoActor1")
+        actorRef ! "hello"
+        expectMsg("HELLO")
+      }
+      enterBarrier("job-done")
+    }
+  }
+}
+
+class DemoActor1 extends Actor {
+  override def receive = {
+    case x => sender ! x.toString.toUpperCase
+  }
+}
